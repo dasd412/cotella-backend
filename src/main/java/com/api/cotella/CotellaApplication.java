9f7873d520165c77bbd31d4bdd8392393b7b3a59
yang-yeongjun <dasd412@naver.com>
2024-01-01 14:33:13 +0900

    chore : add .sql ignore

---
+++ src/main/java/com/api/cotella/CotellaApplication.java
@@ -0,0 +1,13 @@
+package com.api.cotella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotellaApplication.class, args);
+	}
+
+}
