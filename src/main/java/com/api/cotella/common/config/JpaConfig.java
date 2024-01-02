fc8510d41a67a3d62cab78c805de7a4c68bdfb8b
yang-yeongjun <dasd412@naver.com>
2024-01-02 19:00:52 +0900

    chore : querydsl update for javax->jakarta

--- src/main/java/com/api/cotella/common/config/JpaConfig.java
+++ src/main/java/com/api/cotella/common/config/JpaConfig.java
@@ -1,8 +1,9 @@
 package com.api.cotella.common.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
