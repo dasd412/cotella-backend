df2837fdd18aa5576ec3e79db7f1f4a6f7761597
yang-yeongjun <dasd412@naver.com>
2024-01-03 19:42:25 +0900

    feat : add n:1 one direction mapping between InterviewQuestion and InterviewKeyword

--- src/main/java/com/api/cotella/model/question/InterviewQuestion.java
+++ src/main/java/com/api/cotella/model/question/InterviewQuestion.java
@@ -1,9 +1,56 @@
 package com.api.cotella.model.question;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Objects;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "interview_question")
 public class InterviewQuestion {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "interview_question_id")
   private Long id;
-  private String questionContent;
-  private String keywordId;
 
+  private String questionContent;
+
+  @ManyToOne
+  @JoinColumn(name = "interview_keyword_id")
+  private InterviewKeyword interviewKeyword;
+
+  public InterviewQuestion(){
+  }
+
+  @Builder
+  public InterviewQuestion(String questionContent, InterviewKeyword interviewKeyword){
+    this.questionContent=questionContent;
+    this.interviewKeyword=interviewKeyword;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    InterviewQuestion target = (InterviewQuestion) obj;
+    return Objects.equals(this.id, target.id);
+  }
 }
