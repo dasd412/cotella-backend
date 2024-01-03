3a52a1ed35a323e2678fbfa7c52cc80ccf8a1022
yang-yeongjun <dasd412@naver.com>
2024-01-03 20:47:24 +0900

    feat : add n:1 one direction mapping between FitInterviewAnswerEditing and Answer

--- src/main/java/com/api/cotella/model/llm/FitInterviewAnswerEditing.java
+++ src/main/java/com/api/cotella/model/llm/FitInterviewAnswerEditing.java
@@ -1,8 +1,57 @@
 package com.api.cotella.model.llm;
 
+import com.api.cotella.model.answer.Answer;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Objects;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "fit_interview_answer_editing")
 public class FitInterviewAnswerEditing {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "fit_interview_answer_editing_id")
   private Long id;
+
   private String content;
-  private Long answerId;
+
+  @ManyToOne
+  @JoinColumn(name = "answer_id")
+  private Answer answer;
+
+  public FitInterviewAnswerEditing() {
+  }
+
+  @Builder
+  public FitInterviewAnswerEditing(String content, Answer answer) {
+    this.content = content;
+    this.answer = answer;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    FitInterviewAnswerEditing target = (FitInterviewAnswerEditing) obj;
+    return Objects.equals(this.id, target.id);
+  }
 }
