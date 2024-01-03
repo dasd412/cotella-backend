f53e538d07dcf54e1f8b000a88d6584a3c38eeac
yang-yeongjun <dasd412@naver.com>
2024-01-03 20:24:32 +0900

    feat : add n:n one direction mapping between InterviewSession and InterviewQuestion by Answer

--- src/main/java/com/api/cotella/model/answer/Answer.java
+++ src/main/java/com/api/cotella/model/answer/Answer.java
@@ -1,11 +1,70 @@
 package com.api.cotella.model.answer;
 
+import com.api.cotella.model.question.InterviewQuestion;
+import com.api.cotella.model.session.InterviewSession;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Objects;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "answer")
 public class Answer {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "answer_id")
   private Long id;
+
   private String answerContent;
+
   private int likesCount;
+
   private int dislikesCount;
-  private Long questionId;
-  private Long interviewSessionId;
+
+  @ManyToOne
+  @JoinColumn(name = "interview_question_id")
+  private InterviewQuestion interviewQuestion;
+
+  @ManyToOne
+  @JoinColumn(name = "interview_session_id")
+  private InterviewSession interviewSession;
+
+  public Answer() {
+  }
+
+  @Builder
+  public Answer(String answerContent, int likesCount, int dislikesCount,
+      InterviewQuestion interviewQuestion, InterviewSession interviewSession) {
+    this.answerContent = answerContent;
+    this.likesCount = likesCount;
+    this.dislikesCount = dislikesCount;
+    this.interviewQuestion = interviewQuestion;
+    this.interviewSession = interviewSession;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    Answer target = (Answer) obj;
+    return Objects.equals(this.id, target.id);
+  }
 }
