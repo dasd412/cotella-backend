973954ec1384ea8ee2d93c73c3fccf94cfe8f6d0
yang-yeongjun <dasd412@naver.com>
2024-01-03 19:29:48 +0900

    feat : add n:1 one direction mapping between InterviewKeyword and InterviewTopic

--- src/main/java/com/api/cotella/model/question/InterviewKeyword.java
+++ src/main/java/com/api/cotella/model/question/InterviewKeyword.java
@@ -1,8 +1,60 @@
 package com.api.cotella.model.question;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Objects;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "interview_keyword")
 public class InterviewKeyword {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "interview_keyword_id")
   private Long id;
-  private String keywordContent;
-  private Long interviewTopicId;
+
+  @Enumerated(EnumType.STRING)
+  private InterviewKeywordContent keywordContent;
+
+  @ManyToOne
+  @JoinColumn(name = "interview_topic_id")
+  private InterviewTopic interviewTopic;
+
+  public InterviewKeyword() {
+  }
+
+  @Builder
+  public InterviewKeyword(InterviewKeywordContent interviewKeywordContent,
+      InterviewTopic interviewTopic) {
+    this.keywordContent = interviewKeywordContent;
+    this.interviewTopic = interviewTopic;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    InterviewKeyword target = (InterviewKeyword) obj;
+    return Objects.equals(this.id, target.id);
+  }
 }
--- src/main/java/com/api/cotella/model/question/InterviewTopic.java
+++ src/main/java/com/api/cotella/model/question/InterviewTopic.java
@@ -1,8 +1,50 @@
 package com.api.cotella.model.question;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Objects;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "interview_topic")
 public class InterviewTopic {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "interview_topic_id")
   private Long id;
-  private String interviewCategory;
 
+  @Enumerated(EnumType.STRING)
+  private InterviewCategory interviewCategory;
+
+  public InterviewTopic() {
+  }
+
+  public InterviewTopic(InterviewCategory interviewCategory) {
+    this.interviewCategory = interviewCategory;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    InterviewTopic target = (InterviewTopic) obj;
+    return Objects.equals(this.id, target.id);
+  }
 }
